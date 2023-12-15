fdb7d8294d386d2f2886653d57111740a2b697fc
ManoherG7 <gaykwadmanoj17@gmail.com>
2023-12-15 21:56:26 +0530

    added new changes

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,25 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+// import './index.css'
+import { BrowserRouter } from "react-router-dom";
+import { ChakraProvider } from "@chakra-ui/react";
+// import { Auth0Provider } from "@auth0/auth0-react";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <ChakraProvider>
+      <BrowserRouter>
+        {/* <Auth0Provider
+          domain="dev-ni1cmgs0cu6e2atq.us.auth0.com"
+          clientId="8U93T854YyFDfmlND4WGNVwqA68JRfSK"
+          redirectUri={window.location.origin}
+        > */}
+          <App />
+        {/* </Auth0Provider> */}
+      </BrowserRouter>
+    </ChakraProvider>
+  </React.StrictMode>
+);
+
+
---
+++ src/pages/AddVehicle.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Dashboard from "../pages/Dashboard/Dashboard";
+
+const AddVehicle = () => {
+  return (
+    <>
+    <Dashboard />
+      <div style={{marginLeft:"260px", marginTop:"20px"}}>
+
+        <h1>Add Vehicle</h1>
+      </div>
+    </>
+  );
+};
+
+export default AddVehicle;
