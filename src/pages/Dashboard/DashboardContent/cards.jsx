92695b8257b9d2ced1ba2001115042b33479e1a7
ManoherG7 <gaykwadmanoj17@gmail.com>
2023-12-18 09:56:26 +0530

    added dashboard

---
+++ src/pages/Dashboard/DashboardContent/cards.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Dashboard from "../pages/Dashboard/Dashboard";
+
+const card1 = () => {
+  return (
+    <>
+    <Dashboard />
+      <div style={{marginLeft:"260px", marginTop:"20px"}}>
+
+        <h1>Cards</h1>
+      </div>
+    </>
+  );
+};
+
+export default card1;
